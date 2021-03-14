9e824a6ff68226e3b35a6aa17ec9eadde423e637
db-tec <eng.davidborges@gmail.com>
2021-03-13 22:23:26 -0300

    pagina inscrição

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from .views import index, fazer_inscricao
 
 urlpatterns = [
     path('', index, name='index'),
-    path('inscricao/', fazer_inscricao, name="fazer_inscricao")
+    path('inscricao/', fazer_inscricao, name="inscricao")
 ]
